506dbef147b18d0c5aa114f58b54b0573a36161b
Rohan Sakhale <rohansakhale@gmail.com>
2024-05-13 12:33:48 +0530

    Output export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
    distDir: 'dist',
     images: {
       domains: ['w7.pngwing.com','www.spectrumspace.org.au'], // Add any other domains you need here
